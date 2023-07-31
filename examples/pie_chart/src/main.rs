f40d5c2aff46cdb4480b0cc16bfb8ef68713c781
LangPham <phamxuanlang@gmail.com>
2023-07-31 22:09:26 +0700

    Edit piechart

--- examples/pie_chart/src/main.rs
+++ examples/pie_chart/src/main.rs
@@ -3,18 +3,18 @@ use leptos_chart::*;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    leptos::mount_to_body(|cx| leptos::view! { cx,  <App/> })
+    leptos::mount_to_body(|| leptos::view! { <App/> })
 }
 
 #[component]
-pub fn App(cx: Scope) -> impl IntoView {
+pub fn App() -> impl IntoView {
     let chart = Polar::new(
         Series::from(vec![1.0, 2.0, 5.]),
         Series::from(vec!["A", "B", "C"]),
     )
     .set_view(740, 540, 1, 200, 20);
 
-    view! {cx,
+    view! {
         <div class="mx-auto p-8">
             <h1>"Pie chart example with right label"</h1>
             <PieChart chart=chart />
