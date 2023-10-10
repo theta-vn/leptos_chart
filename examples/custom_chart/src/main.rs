811be287efbe1d138e98d524b3ef9aa72181e692
martinfrances107 <martinfrances107@hotmail.com>
2023-10-10 20:40:40 +0100

    A more complex custom_chart example.
    A custom SvgChart is combined with a third party module d3_delaunary_rs
    to produce a deluany triangulation.
    
    https://en.wikipedia.org/wiki/Delaunay_triangulation

--- examples/custom_chart/src/main.rs
+++ examples/custom_chart/src/main.rs
@@ -1,3 +1,5 @@
+use d3_delaunay_rs::delaunay::Delaunay;
+use geo_types::Coord;
 use leptos::*;
 use leptos_chart::*;
 use theta_chart::color::Color;
@@ -10,9 +12,11 @@ fn main() {
 
 #[component]
 pub fn App() -> impl IntoView {
+    let x_values = vec![50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+    let y_values = vec![7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
     let chart = Cartesian::new(
-        Series::from(vec![50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]).set_range(40., 160.),
-        Series::from(vec![7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]).set_range(6., 16.),
+        Series::from(x_values.clone()).set_range(40., 160.),
+        Series::from(y_values.clone()).set_range(6., 16.),
     )
     .set_view(820, 620, 3, 100, 100, 20);
 
@@ -52,45 +56,61 @@ pub fn App() -> impl IntoView {
     let xsticks = xseries.to_stick();
     let ysticks = yseries.to_stick();
 
+    // Convert data series into a form compatible with the d3_delauany crate.
+    let vector = rec_chart.get_vector();
+    let points = x_values
+        .iter()
+        .zip(y_values)
+        .map(|(x, y)| Coord {
+            x: xseries.scale(*x as f64) * vector.get_x(),
+            y: yseries.scale(y as f64) * vector.get_y(),
+        })
+        .collect::<Vec<Coord<f64>>>();
+
+    let delaunay = Delaunay::new(&points);
+    let d_delaunay = delaunay.render_to_string();
+
     view! {
         <div class="mx-auto p-8">
-            <h1>"Scatter chart example"</h1>
+            <h1>"The delaunay triangulation of a set of points"</h1>
             <SvgChart cview >
-            <g class="axes">
-            <g class="x-axis" transform={translate_xa}>
-            <XAxis region=rec_xa axes=axes_x />
-            </g>
-            <g class="y-axis" transform={translate_ya}>
-            <YAxis region=rec_ya axes=axes_y />
-            </g>
-            </g>
-            <g class="inner-chart"  transform={translate_chart}>
-            // For draw region of chart
-              <text x="50" y="-400">"Custom Title"</text>
-             {
-                  #[cfg(feature = "debug")]
-                  {
-                      let vector = rec_chart.get_vector();
-                      let path = format!("M {},{} l {},{} l {},{} l {},{} Z", 0, 0, vector.get_x(), 0, 0,vector.get_y(), -vector.get_x(), 0);
-                      view! {
-                          <circle id="origin" cx="0" cy="0" r="3" />
-                          <line x1="0" y1="0" x2=vector.get_x() y2=vector.get_y() style="stroke:#00ff0033;stroke-width:2" />
-                          <path id="region" d=path  fill="#00ff0033" />
-                      }
-                  }
-              }
-              {
-                  let color = Color::default();
-                  let vector = rec_chart.get_vector();
-                  xsticks.into_iter().enumerate().map(|(index, data)|  {
-                      let x: f64 = xseries.scale(data.value) * vector.get_x();
-                      let y: f64 = yseries.scale(ysticks[index].value) *vector.get_y();
-                      view! {
-                          <circle cx={x} cy={y}  r="4" fill=color.to_string_hex() />
-                      }
-                  }).collect::<Vec<_>>()
-              }
-          </g>
+              <g class="axes">
+              <g class="x-axis" transform={translate_xa}>
+              <XAxis region=rec_xa axes=axes_x />
+              </g>
+              <g class="y-axis" transform={translate_ya}>
+              <YAxis region=rec_ya axes=axes_y />
+              </g>
+              </g>
+              <g class="inner-chart"  transform={translate_chart}>
+              // For draw region of chart
+               {
+                    #[cfg(feature = "debug")]
+                    {
+                        let vector = rec_chart.get_vector();
+                        let path = format!("M {},{} l {},{} l {},{} l {},{} Z", 0, 0, vector.get_x(), 0, 0,vector.get_y(), -vector.get_x(), 0);
+                        view! {
+                            <circle id="origin" cx="0" cy="0" r="3" />
+                            <line x1="0" y1="0" x2=vector.get_x() y2=vector.get_y() style="stroke:#00ff0033;stroke-width:2" />
+                            <path id="region" d=path  fill="#00ff0033" />
+                        }
+                    }
+                }
+
+                <path class="delauany" d=d_delaunay stroke="green" fill="none"/>
+
+                {
+                    let color = Color::default();
+                    let vector = rec_chart.get_vector();
+                    xsticks.into_iter().enumerate().map(|(index, data)|  {
+                        let x: f64 = xseries.scale(data.value) * vector.get_x();
+                        let y: f64 = yseries.scale(ysticks[index].value) *vector.get_y();
+                        view! {
+                            <circle cx={x} cy={y}  r="4" fill=color.to_string_hex() />
+                        }
+                    }).collect::<Vec<_>>()
+                }
+              </g>
             </SvgChart>
 
         </div>
